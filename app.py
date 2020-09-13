a16852633d8b29a702689f72a27f24c03fba9b5f
Justin Miller <justinmiller171@gmail.com>
2020-09-13 16:24:53 -0700

    added base.html and formatted other pages

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-  return render_template("index.html", title="Home", content="This is the home page.")
+    return render_template("index.html", title="Home", content="This is the home page.")
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
