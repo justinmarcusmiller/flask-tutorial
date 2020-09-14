2def5f5979798354cd932e3b1649cb552bc30956
Justin Miller <justinmiller171@gmail.com>
2020-09-14 13:23:01 -0700

    Added flashing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
-
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
 
 @app.route("/")
 def home():
@@ -10,14 +12,32 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        flash("Login Succesful!")
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash("You have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run()
